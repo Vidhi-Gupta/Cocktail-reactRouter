86f61cb0fcf03ae9d1537e8a71a7101bc73eaa1d
Vidhi-Gupta <37909623+Vidhi-Gupta@users.noreply.github.com>
2021-02-15 19:32:17 +0530

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nostrum unde incidunt! Nam inventore iste, harum doloremque nostrum eos tempore eligendi quidem incidunt deleniti hic saepe! Commodi officia consequatur, a nostrum qui modi dolore ipsum quidem iusto, voluptates magni atque!
+    </p>
+    </section>
+    
+  )
+}
+
+export default About
